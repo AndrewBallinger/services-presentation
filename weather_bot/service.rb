a4c9260752b7387abc94195ef337d52d92a3b673
Michael Stock <mikeastock@gmail.com>
2015-03-12 23:45:50 +0000

    Swap to using HTTParty

--- weather_bot/service.rb
+++ weather_bot/service.rb
@@ -1,16 +1,16 @@
-require 'sinatra'
-require 'forecast_io'
-require 'json'
-require 'net/http'
+require "sinatra"
+require "forecast_io"
+require "json"
+require "httparty"
 
-ForecastIO.api_key = '0b11f1a03028060fa9bddab0b4390855'
-post '/' do
+ForecastIO.api_key = "0b11f1a03028060fa9bddab0b4390855"
+post "/" do
   json = JSON.parse(weather)
   today = json["daily"]["data"].first
   max = today["temperatureMax"].to_s
 
   text = "The high is " + max + " degrees in Carpenteria today!"
-  
+
   if params[:channel_name].include?("support")
     text += " Bet you wish you were here!"
   end
@@ -18,22 +18,19 @@ post '/' do
   data = {
     channel: "#" + params[:channel_name],
     text: text
-  }.to_json
+  }
 
-  url = URI.parse('https://hooks.slack.com/services/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')
-  req = Net::HTTP::Post.new(url.to_s)
-  req.body = data.to_s
-  req.content_type = 'application/json'
+  url = "https://hooks.slack.com/services/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"
+  response = HTTParty.get(url, body: data.to_json)
 
-  http = Net::HTTP.new(url.host, url.port)
-
-  http.use_ssl = true
-  res = http.request(req)
-  res.body
-  
+  if response.success?
+    response.body
+  else
+    raise BadRequest, response.body
+  end
 end
 
-get '/data' do
+get "/data" do
   weather.to_s
 end
 
@@ -41,4 +38,4 @@ def weather
   (ForecastIO.forecast 34.398,-119.518).to_json
 end
 
-
+class BadRequest < StandardError; end
